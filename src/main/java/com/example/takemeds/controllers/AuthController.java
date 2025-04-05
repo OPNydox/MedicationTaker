2fcbcc93173d1ec4f79307cd62c609d42df59c4c
unknown <cbotushanov@gmail.com>
2025-04-06 01:27:06 +0300

    Fixing bug where login endpoint not being detected

--- src/main/java/com/example/takemeds/controllers/AuthController.java
+++ src/main/java/com/example/takemeds/controllers/AuthController.java
@@ -10,12 +10,14 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
@@ -25,7 +27,7 @@ public class AuthController {
         this.authenticationManager = authenticationManager;
     }
 
-    @PostMapping(name = "/api/auth/login")
+    @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {
         String username = credentials.get("username");
         String password = credentials.get("password");
