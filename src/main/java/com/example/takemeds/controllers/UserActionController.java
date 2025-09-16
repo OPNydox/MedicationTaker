8cbf66b6463047012fdf9ff5570add535baa9ead
unknown <cbotushanov@gmail.com>
2025-09-16 22:25:49 +0300

    Depricating user action service

--- src/main/java/com/example/takemeds/controllers/UserActionController.java
+++ src/main/java/com/example/takemeds/controllers/UserActionController.java
@@ -1,24 +1,15 @@
 package com.example.takemeds.controllers;
 
-import com.example.takemeds.exceptions.InvalidFrequencyException;
 import com.example.takemeds.presentationModels.MedLogPresentationModel;
-import com.example.takemeds.presentationModels.dosagePMs.CreateDosagePM;
-import com.example.takemeds.presentationModels.medicationPMs.CreateMedicationDto;
 import com.example.takemeds.presentationModels.medicationPMs.MedicationView;
 import com.example.takemeds.services.MedicationLogService;
-import com.example.takemeds.services.UserActionService;
 import com.example.takemeds.services.UserService;
-import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,12 +23,9 @@ public class UserActionController {
 
     private final UserService userService;
 
-    private final UserActionService userActionService;
-
-    public UserActionController(MedicationLogService medicationLogService, UserService userService, UserActionService userActionService) {
+    public UserActionController(MedicationLogService medicationLogService, UserService userService) {
         this.medicationLogService = medicationLogService;
         this.userService = userService;
-        this.userActionService = userActionService;
     }
 
     @PostMapping("/take/{id}")
